d1b3d6538423c3a5c0e9b458398f0c4ae50cef7b
Bild <flaviafernand.moraes@gmail.com>
2021-10-18 11:05:24 -0300

    Initial commit

---
+++ src/layouts/TodoListLayout.vue
@@ -0,0 +1,29 @@
+<template>
+<div>HELLO VUE WORLD</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
+
+export default defineComponent({
+  name: 'TodoList',
+
+  setup () {
+    const $q = useQuasar()
+    console.log($q.dark.set(false))
+  }
+})
+</script>
+
+<style lang='scss'>
+body.body--dark {
+  background-image: url("~assets/todolist/bg-desktop-dark.jpg");
+  background-repeat: repeat-x;
+}
+body.body--light {
+  background-image: url("~assets/todolist/bg-desktop-light.jpg");
+  background-repeat: repeat-x;
+  background-color: $light;
+}
+</style>
